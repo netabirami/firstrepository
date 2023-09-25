f946ed30331ad02b135d30018628c60ae7ef1976
netabirami <net.abirami@gmail.com>
2023-09-25 22:39:17 +0200

    first project commit

---
+++ src/Demo/Animal.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Animal {
+    String type;
+    String size;
+    double weight;
+
+    public Animal(String type, String size, double weight, String bark) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+}
